c36e289c442df4519937226a8054486bafbe5b56
zhongtianzhou <zhongtianzhou@360.cn>
2023-01-10 18:32:25 +0800

    test build

--- build2/config.sh
+++ build2/config.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
 # enableShared=[true/false]
-reBuildDeps=false # 是否重新编译(是否重新编译依赖库配置)
+reBuildDeps=true # 是否重新编译(是否重新编译依赖库配置)
 enableShared=false # 是否开启动态库(依赖库配置)
-enableSharedFFmpeg=true # 是否开启动态库(FFmpeg配置)
+enableSharedFFmpeg=false # 是否开启动态库(FFmpeg配置)
